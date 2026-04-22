634da2e426133be7e049977303d7fa4e9aeaaf5a
Tony <your@email.com>
2026-04-22 23:00:38 +0300

    update web

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4152943246" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3530890185" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
